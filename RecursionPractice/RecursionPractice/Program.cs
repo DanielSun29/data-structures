3ffa2e7205546a8f5659ca66dd197c86968c52f1
DanielSun29 <danielcysun@gmail.com>
2025-11-11 19:05:49 -0800

    Added Recursion Practice Code

---
+++ RecursionPractice/RecursionPractice/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace RecursionPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{Divide(12,3)}");
+        }
+        static int Multiply(int x, int y)
+        {
+            int ans = 0;
+
+            if (y < 0)
+            {
+                return Multiply(-x, -y);
+            }
+
+            if (y < 1)
+            {
+                return ans;
+            }
+
+            ans = x + Multiply(x, y - 1);
+            return ans;
+        }
+        static int Divide(int x, int y)
+        {
+            if (y == 0)
+            {
+                throw new ArgumentException("divided by 0");
+            }
+
+            int ans = 0;
+
+            if (x <= 0)
+            {
+                return ans;
+            }
+
+            ans = 1 + Divide(x - y, y);
+            return ans;
+        }
+    }
+}
