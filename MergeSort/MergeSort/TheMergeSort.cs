cb3da9ab7aab19bd4ced4facf5c534a8d1ff918d
DanielSun29 <danielcysun@gmail.com>
2025-11-16 12:00:46 -0800

    docs: Started MergeSort

---
+++ MergeSort/MergeSort/TheMergeSort.cs
@@ -0,0 +1,20 @@
+﻿namespace MergeSort
+{
+    public class TheMergeSort<T> where T: IComparable<T>
+    {
+        public static List<T> Sort(List<T> list)
+        {
+            //This is the only function with recursion, the other 2 are helper functions
+            return null;
+        }
+        static List<T> Merge(List<T> left, List<T> right)
+        {
+            return null;
+        }
+        static (List<T> Left,List<T> Right) Split(List<T> list)
+        {
+            return (null,null);
+        }
+    }
+    
+}
