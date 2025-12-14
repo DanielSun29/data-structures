13e4567cfe2f30d0d4af97eded62bfaaf04b616a
DanielSun29 <Daniel.Sun@GMR-118-2-3>
2025-12-14 10:45:03 -0800

    added some quick sort code

--- QuickSort/QuickSort/TheQuickSort.cs
+++ QuickSort/QuickSort/TheQuickSort.cs
@@ -2,14 +2,74 @@
 {
     public class TheQuickSort <T> where T : IComparable<T>
     {
-        public static List<T> LomutoSort(List<T> list)
+        public static void LomutoSort(T[] arr, int start, int end) // end is inclusive
         {
-            return null;
+            if (end - start < 1)
+            {
+                return;
+            }
+
+            var pivot = arr[end];
+            int wall = start;
+            int curr = start;
+            while (curr < end)
+            {
+                if (wall >= arr.Length)
+                {
+                    return;
+                }
+                if (arr[curr].CompareTo(pivot) < 0)
+                {
+                    Swap(arr, curr, wall);
+                    wall++;
+                    curr++;
+                }
+                else 
+                {
+                    curr++;
+                }
+            }
+            
+            Swap(arr, end, wall);
+            
+            LomutoSort(arr, start, wall - 1);
+            LomutoSort(arr, wall + 1, end);
         }
 
-        public static List<T> HoareSort(List<T> list)
+        public static void HoareSort(T[] arr, int start, int end)
         {
-            return null;
+            if (end - start < 1)
+            {
+                return;
+            }
+            int left = start - 1;
+            int right = end + 1;
+            var pivot = arr[start];
+            while (left < right)
+            {
+                do
+                {
+                    left++;
+                } while (arr[left].CompareTo(pivot) < 0 && left < right);
+
+                do
+                {
+                    right--;
+                } while (arr[right].CompareTo(pivot) > 0 && left < right);
+                if (left < right)
+                {
+                    Swap(arr, left, right);
+                }
+            }
+            HoareSort(arr, start, right);
+            HoareSort(arr, right + 1, end);
+        }
+
+        private static void Swap(T[] arr, int i, int j)
+        {
+            var temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
     }
 }
--- QuickSort/QuickSortTest/QuickSortTest.cs
+++ QuickSort/QuickSortTest/QuickSortTest.cs
@@ -1,11 +1,59 @@
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using QuickSort;
+
 namespace TheQuickSortTesting
 {
     public class QuickSortTest
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(new int[] { 7, 4, 3, 6, 2, 1, 5})]
+        [InlineData(new int[] { 7, 0, 0, 0})]
+        [InlineData(new int[] { 6, 27, 58, 26, 61})]
+        public void LomutoSortTest(int[] arr)
         {
+            int[] temp = new int[arr.Length];
+            Array.Copy(arr, temp, arr.Length);
 
+            TheQuickSort<int>.LomutoSort(arr, 0, arr.Length - 1);
+
+            Array.Sort(temp);
+            Assert.True(temp.SequenceEqual(arr));
+        }
+
+        [Theory]
+        [InlineData(new int[] { 7, 4, 3, 6, 2, 1, 5 })]
+        [InlineData(new int[] { 7, 0, 0, 0 })]
+        [InlineData(new int[] { 6, 27, 58, 26, 61 })]
+        public void HoareSortTest(int[] arr)
+        {
+            int[] temp = new int[arr.Length];
+            Array.Copy(arr, temp, arr.Length);
+
+            TheQuickSort<int>.HoareSort(arr, 0, arr.Length - 1);
+
+            Array.Sort(temp);
+            Assert.True(temp.SequenceEqual(arr));
+        }
+
+        [Fact]
+        public void StressTest()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                int[] og = new int[100];
+                for (int j = 0; j < 100; j++)
+                {
+                    og[j] = Random.Shared.Next(0, 100);
+                }
+                int[] output = new int[100];
+                Array.Copy(og, output, og.Length);
+                TheQuickSort<int>.LomutoSort(output, 0, 99);
+
+                for (int j = 0; j < 99; j++)
+                {
+                    Assert.True(output[j] <= output[j+1]);
+                }
+            }
         }
     }
 }
