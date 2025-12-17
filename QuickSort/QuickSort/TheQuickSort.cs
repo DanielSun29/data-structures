374ce6e88b39e1ab722a8228c4b02807fa6155b0
Daniel.Sun <danielcysun@gmail.com>
2025-12-16 18:59:01 -0800

    added some QuickSort code

--- QuickSort/QuickSort/TheQuickSort.cs
+++ QuickSort/QuickSort/TheQuickSort.cs
@@ -42,9 +42,9 @@
             {
                 return;
             }
-            int left = start;
-            int right = end;
-            var pivot = arr[start];
+            //int left = start;
+            //int right = end;
+            //var pivot = arr[start];
             //while (left < right)
             //{
             //    while (arr[left].CompareTo(pivot) < 0 && left < right)
@@ -72,17 +72,22 @@
             //    HoareSort(arr, start, right);
             //}
             //HoareSort(arr, right + 1, end);
-            //// The problem here to fix is that, if we don't increase left, its gonna be forever stuck when encountering multiple same values, 
-            //// if we do increase left, there is a probability to skip thru data that still needs to be sorted. Same with decreasing right.
-            //// This problem is not solved yet, and for now the commented out code is the same as the code that are not(cuz i dont know what to do yet)
+            // The problem here to fix is that, if we don't increase left, its gonna be forever stuck when encountering multiple same values, 
+            // if we do increase left, there is a probability to skip thru data that still needs to be sorted. Same with decreasing right.
+            // This problem is not solved yet, and for now the commented out code is the same as the code that are not(cuz i dont know what to do yet)
+            int left = start - 1;
+            int right = end + 1;
+            var pivot = arr[start];
             while (left < right)
             {
-                while (arr[left].CompareTo(pivot) < 0 && left < right)
+                left++;
+                while (arr[left].CompareTo(pivot) < 0)
                 {
                     left++;
                 }
 
-                while (arr[right].CompareTo(pivot) > 0 && left < right)
+                right--;
+                while (arr[right].CompareTo(pivot) > 0)
                 {
                     right--;
                 }
@@ -93,6 +98,37 @@
                     left++;
                 }
             }
+            //T pivot = arr[start];
+            //int left = start - 1;
+            //int right = end + 1;
+
+            ////keep looping till the indicators over lap with one another
+            //while (true)
+            //{
+            //    //keep increasing the left indicator till the value is bigger or equal to the pivot
+            //    do
+            //    {
+            //        left++;
+            //    } while (arr[left].CompareTo(pivot) < 0);
+
+            //    //keep decreasing the right indicator till the value is smaller or equal to the pivot
+            //    do
+            //    {
+            //        right--;
+            //    } while (arr[right].CompareTo(pivot) > 0);
+
+            //    //overlap check
+            //    if (left >= right)
+            //    {
+            //        break;
+            //    }
+
+            //    //swap the indicators when you finish moving them
+            //    Swap(arr, left, right);
+            //}
+
+            //#################################################
+            // This part is likely wrong since 2 of the test cases were wrong w/ the github code and this was the part we had.
             if (right == end)
             {
                 HoareSort(arr, start, right - 1);
