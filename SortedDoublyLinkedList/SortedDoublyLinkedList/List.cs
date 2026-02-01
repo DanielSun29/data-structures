900ae9911251c3b532a1555a6481b4e6247baa2d
DanielSun29 <danielcysun@gmail.com>
2026-02-01 12:02:49 -0800

    Added doubly linked list and insert

---
+++ SortedDoublyLinkedList/SortedDoublyLinkedList/List.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortedDoublyLinkedList
+{
+    internal class List<T> where T : IComparable<T>
+    {
+        public Node<T> Head { get; set; }
+
+        public int Count { get; set; }
+
+        public List()
+        {
+            Head = new Node<T>(default(T),null,null);
+            Count = 0;
+        }
+
+
+        private void ConnectNodes(Node<T> previous, Node<T> newNode, Node<T> next)
+        {
+            newNode.Next = next;
+            if (next != null)
+            {
+                next.Previous = newNode;
+            }
+            newNode.Previous = previous;
+            previous.Next = newNode;
+        }
+
+
+        public void Add(T value)
+        {
+            Node<T> temp = new Node<T>(value);
+            Node<T> curr = Head;
+            while (curr.Next != null && curr.Next.Value.CompareTo(value) < 0)
+            {
+                curr = curr.Next;
+            }
+            ConnectNodes(curr, temp, curr.Next);
+            Count++;
+        }
+    }
+}
---
+++ SortedDoublyLinkedList/SortedDoublyLinkedList/Node.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortedDoublyLinkedList
+{
+    internal class Node<T> where T : IComparable<T>
+    {
+        public Node<T> Next { get; set; }
+        public Node<T> Previous { get; set; } // the only change
+
+        public T Value { get; set; }
+
+        public Node(T value)
+        {
+            Value = value;
+            Next = null;
+            Previous = null;
+        }
+
+        public Node(T value, Node<T> next)
+        {
+            Value = value;
+            Next = next;
+            Previous = null;
+        }
+
+        public Node(T value, Node<T> next, Node<T> previous)
+        {
+            Value = value;
+            Next = next;
+            Previous = previous;
+        }
+    }
+}
