7f6568df81c8ad770fcdfc9e4b5267687520d4b4
DanielSun29 <Daniel.Sun@GMR-118-2-3>
2026-02-24 18:43:58 -0800

    Finished Weighted Directed Graphs

--- WeightedDirectedGraphs/WeightedDirectedGraphs/Graph.cs
+++ WeightedDirectedGraphs/WeightedDirectedGraphs/Graph.cs
@@ -8,24 +8,39 @@ namespace WeightedDirectedGraphs
     {
         private List<Vertex<T>> vertices;
 
-        private List<Edge<T>> edges;
+        private List<Edge<T>> GetAllEdges()
+        {
+            List<Edge<T>> edges = new List<Edge<T>>();
+            foreach (Vertex<T> vertex in vertices)
+            {
+                foreach (Edge<T> edge in vertex.Edges)
+                {
+                    if (!edges.Contains(edge))
+                    {
+                        edges.Add(edge);
+                    }
+                }
+            }
+            return edges;
+        }
 
         public IReadOnlyList<Vertex<T>> Vertices { get { return vertices; } }
-        public IReadOnlyList<Edge<T>> Edges { get { return edges; } }
+        public IReadOnlyList<Edge<T>> Edges { get { return GetAllEdges(); } }
 
         public Graph()
         {
             vertices = new List<Vertex<T>>();
-            edges = new List<Edge<T>>();
         }
 
 
         // Functions:
 
-        public void AddVertex(Vertex<T> vertex)
+        public bool AddVertex(Vertex<T> vertex)
         {
+            if (vertex == null) return false;
+            if (Vertices.Contains(vertex)) return false;
             vertices.Add(vertex);
-
+            return true;
         }
         // - Returns true if the addition succeeded, false otherwise
         //
@@ -33,7 +48,16 @@ namespace WeightedDirectedGraphs
 
         public bool RemoveVertex(Vertex<T> vertex)
         {
+            if (vertex == null) return false;
+            if (!Vertices.Contains(vertex)) return false;
             vertices.Remove(vertex);
+            foreach (Edge<T> edge in Edges)
+            {
+                if (edge.StartVertex == vertex || edge.EndVertex == vertex)
+                {
+                    RemoveEdge(edge.StartVertex, edge.EndVertex);
+                }
+            }
             return true;
         }
         // - Returns true if the removal succeeded, false otherwise
@@ -42,6 +66,9 @@ namespace WeightedDirectedGraphs
 
         public bool AddEdge(Vertex<T> a, Vertex<T> b, float distance)
         {
+            if (!(Vertices.Contains(a) && Vertices.Contains(b))) return false;
+            Edge<T> edge = new Edge<T>(a, b, distance);
+            a.Edges.Add(edge);
             return true;
         }
         // - Returns true if the addition succeeded, false otherwise
@@ -51,6 +78,15 @@ namespace WeightedDirectedGraphs
 
         public bool RemoveEdge(Vertex<T> a, Vertex<T> b)
         {
+            if (!(Vertices.Contains(a) && Vertices.Contains(b))) return false;
+            foreach (Edge<T> edge in a.Edges)
+            {
+                if (edge.EndVertex == b)
+                {
+                    a.Edges.Remove(edge);
+                    break;
+                }
+            }
             return true;
         }
         // - Returns true if the removal succeeded, false otherwise
@@ -59,15 +95,76 @@ namespace WeightedDirectedGraphs
 
         public Vertex<T> Search(T value)
         {
+            foreach (Vertex<T> vertex in vertices)
+            {
+                if (vertex.Value.Equals(value))
+                {
+                    return vertex;
+                }
+            }
             return null;
-
         }
         // - Returns the vertex with the given value, or null if the vertex doesn't exist in the graph.
 
         public Edge<T> GetEdge(Vertex<T> a, Vertex<T> b)
         {
+            foreach (Edge<T> edge in a.Edges)
+            {
+                if (edge.EndVertex == b)
+                {
+                    return edge;
+                }
+            }
             return null;
         }
         // - Returns the edge that connects the two given vertices, or null if the vertex doesn't exist in the graph.
+
+        public bool Contains(T value)
+        {
+            return Search(value) != null;
+        }
+
+        public List<T> DepthFirstTraversal(Vertex<T> curr) => DepthFirstTraversal(curr, []);
+        private List<T> DepthFirstTraversal(Vertex<T> curr, List<Vertex<T>> visited)
+        {
+            List<T> list = new List<T>();
+            visited.Add(curr);
+            foreach (Edge<T> edge in curr.Edges)
+            {
+                Vertex<T> v = edge.EndVertex;
+                if (!visited.Contains(v))
+                {
+                    list.AddRange(DepthFirstTraversal(v, visited));
+                }
+            }
+            list.Add(curr.Value);
+            return list;
+        }
+
+        public List<T> BreadthFirstTraversal(Vertex<T> curr)
+        {
+            List<T> output = new List<T>();
+            List<Vertex<T>> visited = new List<Vertex<T>>();
+            Queue<Vertex<T>> movement = new Queue<Vertex<T>>();
+
+            movement.Enqueue(curr);
+            visited.Add(curr);
+
+            while (movement.Count > 0)
+            {
+                curr = movement.Dequeue();
+                output.Add(curr.Value);
+                foreach (Edge<T> edge in curr.Edges)
+                {
+                    Vertex<T> v = edge.EndVertex;
+                    if (!visited.Contains(v))
+                    {
+                        movement.Enqueue(v);
+                        visited.Add(v);
+                    }
+                }
+            }
+            return output;
+        }
     }
 }
---
+++ WeightedDirectedGraphs/WeightedDirectedGraphsTest/UnitTest1.cs
@@ -0,0 +1,182 @@
+﻿using WeightedDirectedGraphs;
+
+namespace WeightedDirectedGraphsTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void AddTest()
+        {
+            int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7 };
+            Graph<int> graph = new Graph<int>();
+            Vertex<int>[] vertices = new Vertex<int>[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                vertices[i] = new Vertex<int>(arr[i]);
+                graph.AddVertex(vertices[i]);
+                Assert.True(graph.Contains(arr[i]));
+            }
+
+            graph.AddEdge(vertices[0], vertices[2], 1);
+            Assert.Equal(graph.Vertices[0].Edges[0].EndVertex, vertices[2]);
+
+            graph.AddEdge(vertices[1], vertices[2], 1);
+            Assert.Equal(graph.Vertices[1].Edges[0].EndVertex, vertices[2]);
+
+            graph.AddEdge(vertices[3], vertices[5], 1);
+            Assert.Equal(graph.Vertices[3].Edges[0].EndVertex, vertices[5]);
+
+            graph.AddEdge(vertices[3], vertices[7], 1);
+            Assert.Equal(graph.Vertices[3].Edges[1].EndVertex, vertices[7]);
+
+            graph.AddEdge(vertices[6], vertices[5], 1);
+            Assert.Equal(graph.Vertices[6].Edges[0].EndVertex, vertices[5]);
+
+            graph.AddEdge(vertices[6], vertices[7], 1);
+            Assert.Equal(graph.Vertices[6].Edges[1].EndVertex, vertices[7]);
+        }
+
+        [Fact]
+        public void ContainsTest()
+        {
+            int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7 };
+            Graph<int> graph = new Graph<int>();
+            Vertex<int>[] vertices = new Vertex<int>[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                vertices[i] = new Vertex<int>(arr[i]);
+                graph.AddVertex(vertices[i]);
+                Assert.True(graph.Contains(arr[i]));
+            }
+        }
+
+        [Fact]
+        public void AddEdgeTest()
+        {
+            int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7 };
+            Graph<int> graph = new Graph<int>();
+            Vertex<int>[] vertices = new Vertex<int>[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                vertices[i] = new Vertex<int>(arr[i]);
+                graph.AddVertex(vertices[i]);
+                Assert.True(graph.Contains(arr[i]));
+            }
+            graph.AddEdge(vertices[0], vertices[2], 1);
+            Assert.Equal(graph.Vertices[0].Edges[0].EndVertex, vertices[2]);
+        }
+
+        [Fact]
+        public void RemoveTest()
+        {
+            int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7 };
+            Graph<int> graph = new Graph<int>();
+            Vertex<int>[] vertices = new Vertex<int>[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                vertices[i] = new Vertex<int>(arr[i]);
+                graph.AddVertex(vertices[i]);
+                Assert.True(graph.Contains(arr[i]));
+            }
+            graph.AddEdge(vertices[0], vertices[2], 1);
+            graph.AddEdge(vertices[1], vertices[2], 1);
+            graph.AddEdge(vertices[3], vertices[5], 1);
+            graph.AddEdge(vertices[3], vertices[7], 1);
+            graph.AddEdge(vertices[6], vertices[5], 1);
+            graph.AddEdge(vertices[6], vertices[7], 1);
+
+            graph.RemoveVertex(vertices[3]);
+            Assert.False(graph.Contains(3));
+            Assert.DoesNotContain(graph.GetEdge(vertices[3], vertices[5]), graph.Edges);
+            Assert.DoesNotContain(graph.GetEdge(vertices[3], vertices[7]), graph.Edges);
+        }
+
+        [Fact]
+        public void RemoveEdgeTest()
+        {
+            int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7 };
+            Graph<int> graph = new Graph<int>();
+            Vertex<int>[] vertices = new Vertex<int>[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                vertices[i] = new Vertex<int>(arr[i]);
+                graph.AddVertex(vertices[i]);
+                Assert.True(graph.Contains(arr[i]));
+            }
+            graph.AddEdge(vertices[0], vertices[2], 1);
+            graph.AddEdge(vertices[1], vertices[2], 1);
+            graph.AddEdge(vertices[3], vertices[5], 1);
+            graph.AddEdge(vertices[3], vertices[7], 1);
+            graph.AddEdge(vertices[6], vertices[5], 1);
+            graph.AddEdge(vertices[6], vertices[7], 1);
+            graph.RemoveEdge(vertices[3], vertices[5]);
+            Assert.DoesNotContain(graph.GetEdge(vertices[3], vertices[5]), graph.Edges);
+            graph.RemoveEdge(vertices[6], vertices[7]);
+            Assert.DoesNotContain(graph.GetEdge(vertices[6], vertices[7]), graph.Edges);
+        }
+
+        [Fact]
+        public void DepthFirstTraversalTest()
+        {
+            int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7 };
+            Graph<int> graph = new Graph<int>();
+            Vertex<int>[] vertices = new Vertex<int>[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                vertices[i] = new Vertex<int>(arr[i]);
+                graph.AddVertex(vertices[i]);
+                Assert.True(graph.Contains(arr[i]));
+            }
+            graph.AddEdge(vertices[0], vertices[2], 1);
+            graph.AddEdge(vertices[1], vertices[2], 1);
+            graph.AddEdge(vertices[3], vertices[5], 1);
+            graph.AddEdge(vertices[3], vertices[7], 1);
+            graph.AddEdge(vertices[6], vertices[5], 1);
+            graph.AddEdge(vertices[6], vertices[7], 1);
+
+            // Testing
+            Assert.Equal(new List<int> {
+                vertices[2].Value,
+                vertices[0].Value
+            }, graph.DepthFirstTraversal(vertices[0]));
+
+            Assert.Equal(new List<int> {
+                vertices[5].Value,
+                vertices[7].Value,
+                vertices[3].Value
+            }, graph.DepthFirstTraversal(vertices[3]));
+        }
+
+        [Fact]
+        public void BreadthFirstTraversalTest()
+        {
+            int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7 };
+            Graph<int> graph = new Graph<int>();
+            Vertex<int>[] vertices = new Vertex<int>[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                vertices[i] = new Vertex<int>(arr[i]);
+                graph.AddVertex(vertices[i]);
+                Assert.True(graph.Contains(arr[i]));
+            }
+            graph.AddEdge(vertices[0], vertices[2], 1);
+            graph.AddEdge(vertices[1], vertices[2], 1);
+            graph.AddEdge(vertices[3], vertices[5], 1);
+            graph.AddEdge(vertices[3], vertices[7], 1);
+            graph.AddEdge(vertices[6], vertices[5], 1);
+            graph.AddEdge(vertices[6], vertices[7], 1);
+
+            // Testing
+            Assert.Equal(new List<int> {
+                vertices[0].Value,
+                vertices[2].Value,
+            }, graph.BreadthFirstTraversal(vertices[0]));
+
+            Assert.Equal(new List<int> {
+                vertices[3].Value,
+                vertices[5].Value,
+                vertices[7].Value
+            }, graph.BreadthFirstTraversal(vertices[3]));
+        }
+    }
+}
