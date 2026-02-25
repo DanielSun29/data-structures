6c2c5fca5155f0e42dd2d867cc9b939e7df4ddf1
DanielSun29 <Daniel.Sun@GMR-118-2-3>
2026-02-24 18:47:31 -0800

    Added Pathfinding

---
+++ Pathfinding/Pathfinding/Vertex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pathfinding
+{
+    public class Vertex<T>
+    {
+        public T Value { get; set; }
+        public List<Edge<T>> Edges { get; set; }
+
+        public Vertex(T value)
+        {
+            Value = value;
+            Edges = new List<Edge<T>>();
+        }
+    }
+}
---
+++ Pathfinding/Pathfinding/VertexInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pathfinding
+{
+    public class VertexInfo<T>
+    {
+        Vertex<T> Vertex { get; set; } // This is the original vertex
+
+        float TotalCost { get; set; }
+        bool IsVisited { get; set; }
+        Edge<T> FoundingEdge { get; set; }
+            
+        T Value
+        {
+            get
+            {
+                return Vertex.Value;
+            }
+            set
+            {
+                Vertex.Value = value;
+            }
+        }
+    }
+}
