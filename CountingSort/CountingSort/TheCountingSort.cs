529a5d97bacc3165d0720cfdb706826d38fd8deb
DanielSun29 <Daniel.Sun@GMR-118-2-3>
2026-03-24 18:50:01 -0700

    Added and Completed Counting Sort

---
+++ CountingSort/CountingSort/TheCountingSort.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CountingSort
+{
+    public class TheCountingSort<T>
+    {
+        public static List<int> IntSort(List<int> values)
+        {
+            int min = values.Min();
+            int max = values.Max();
+            int range = max - min + 1;
+            List<int> output = new List<int>();
+            int offset = -min;
+            int[] buckets = new int[range];
+            foreach (int value in values)
+            {
+                buckets[value + offset]++;
+            }
+            for (int i = 0; i < buckets.Length; i++)
+            {
+                int n = buckets[i];
+                for (int j = 0; j < n; j++)
+                {
+                    output.Add(i - offset);
+                }
+            }
+            return output;
+        }
+
+        public static List<KeyValuePair<int, T>> Sort(List<KeyValuePair<int, T>> values)
+        {
+            int min = values.Min(kvp => kvp.Key);
+            int max = values.Max(kvp => kvp.Key);
+            int range = max - min + 1;
+            List<KeyValuePair<int, T>> output = new List<KeyValuePair<int, T>>();
+            int offset = -min;
+            List<T>[] buckets = new List<T>[range];
+            foreach (KeyValuePair<int, T> kvp in values)
+            {
+                if (buckets[kvp.Key + offset] == null)
+                {
+                    buckets[kvp.Key + offset] = new List<T>();
+                }
+                buckets[kvp.Key + offset].Add(kvp.Value);
+            }
+            for (int i = 0; i < buckets.Length; i++)
+            {
+                List<T> bucket = buckets[i];
+                if (bucket == null) continue;
+                foreach (T value in bucket)
+                {
+                    output.Add(new KeyValuePair<int, T>(i - offset, value));
+                }
+            }
+            return output;
+        }
+
+        //void yeet()
+        //{
+        //    KeyValuePair<int, string> bobl;
+        //    bobl.
+        //}
+    }
+}
---
+++ CountingSort/CountingSortTest/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using CountingSort;
+
+namespace CountingSortTest
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 4, 1, 2, 7, 5, 2 }, new int[] { 1, 1, 2, 2, 4, 5, 7 })]
+        [InlineData(new int[] { -1, 4, 1, -2, 7, 5, 2 }, new int[] { -2, -1, 1, 2, 4, 5, 7 })]
+        public void IntCountingSortTest1(int[] ints, int[] expected)
+        {
+            List<int> sorted = new List<int>();
+            sorted = TheCountingSort<int>.IntSort(ints.ToList());
+            Assert.Equal(expected, sorted);
+        }
+
+        [Fact]
+        public void CountingSortTest2()
+        {
+            List<KeyValuePair<int, string>> kvps = new List<KeyValuePair<int, string>>()
+            {
+                new KeyValuePair<int, string>(1, "one"),
+                new KeyValuePair<int, string>(4, "four"),
+                new KeyValuePair<int, string>(1, "uno"),
+                new KeyValuePair<int, string>(2, "two"),
+                new KeyValuePair<int, string>(7, "seven"),
+                new KeyValuePair<int, string>(5, "five"),
+                new KeyValuePair<int, string>(2, "dos")
+            };
+            List<KeyValuePair<int, string>> expected = new List<KeyValuePair<int, string>>()
+            {
+                new KeyValuePair<int, string>(1, "one"),
+                new KeyValuePair<int, string>(1, "uno"),
+                new KeyValuePair<int, string>(2, "two"),
+                new KeyValuePair<int, string>(2, "dos"),
+                new KeyValuePair<int, string>(4, "four"),
+                new KeyValuePair<int, string>(5, "five"),
+                new KeyValuePair<int, string>(7, "seven")
+            };
+            List<KeyValuePair<int, string>> sorted = TheCountingSort<string>.Sort(kvps);
+            Assert.Equal(expected.Count, sorted.Count);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i].Key, sorted[i].Key);
+                Assert.Equal(expected[i].Value, sorted[i].Value);
+            }
+        }
+    }
+}
