fd0dcd4b050b1c273cea1c1423f295da74dbd885
DanielSun29 <Daniel.Sun@GMR-118-2-3>
2026-03-31 18:36:50 -0700

    Created & Completed Radix Sort(int, keyvaluepair, different base)

---
+++ RadixSort/RadixSort/TheRadixSort.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RadixSort
+{
+    public class TheRadixSort<T>
+    {
+        public static List<int> IntSort(List<int> input)
+        {
+            if (input == null || input.Count == 0) return input;
+
+            int min = input.Min();
+            int max = input.Max();
+            int range = max - min + 1;
+            int offset = -min;
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                input[i] += offset;
+            }
+
+            min = input.Min();
+            max = input.Max();
+
+            int exp = 1;
+            while (max / exp > 0)
+            {
+                IntRadixHelper(input, exp);
+                exp *= 10;
+            }
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                input[i] -= offset;
+            }
+
+            return input;
+        }
+
+        private static void IntRadixHelper(List<int> input, int exp)
+        {
+            int n = input.Count;
+            List<int> output = new List<int>(new int[n]);
+            int[] count = new int[10]; // 10 here is radix
+
+            for (int i = 0; i < n; i++)
+            {
+                count[(input[i] / exp) % 10]++;
+            }
+
+            for (int i = 1; i < 10; i++)
+            {
+                count[i] += count[i - 1];
+            }
+
+            for (int i = n - 1; i >= 0; i--) // going backwards
+            {
+                output[count[(input[i] / exp) % 10] - 1] = input[i];
+                count[(input[i] / exp) % 10]--;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                input[i] = output[i];
+            }
+        }
+
+        public static List<KeyValuePair<int, T>> Sort(List<KeyValuePair<int, T>> input)
+        {
+            int min = input.Min(kvp => kvp.Key);
+            int max = input.Max(kvp => kvp.Key);
+
+            int range = max - min + 1;
+            int offset = -min;
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                input[i] = new KeyValuePair<int, T>(input[i].Key + offset, input[i].Value);
+            }
+
+
+            min = input.Min(kvp => kvp.Key);
+            max = input.Max(kvp => kvp.Key);
+
+            int exp = 1;
+            while (max / exp > 0)
+            {
+                RadixHelper(input, exp);
+                exp *= 10;
+            }
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                input[i] = new KeyValuePair<int, T>(input[i].Key - offset, input[i].Value);
+            }
+
+            return input;
+        }
+
+        private static void RadixHelper(List<KeyValuePair<int, T>> input, int exp)
+        {
+            int n = input.Count;
+            List<KeyValuePair<int, T>> output = new List<KeyValuePair<int, T>>(new KeyValuePair<int, T>[n]);
+            int[] count = new int[10]; // 10 here is radix
+
+            for (int i = 0; i < n; i++)
+            {
+                count[(input[i].Key / exp) % 10]++;
+            }
+
+            for (int i = 1; i < 10; i++)
+            {
+                count[i] += count[i - 1];
+            }
+
+            for (int i = n - 1; i >= 0; i--) // going backwards
+            {
+                output[count[(input[i].Key / exp) % 10] - 1] = input[i];
+                count[(input[i].Key / exp) % 10]--;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                input[i] = output[i];
+            }
+        }
+
+        public static List<KeyValuePair<int, T>> VarBaseSort(List<KeyValuePair<int, T>> input, int b) // b is the base
+        {
+            int min = input.Min(kvp => kvp.Key);
+            int max = input.Max(kvp => kvp.Key);
+
+            int range = max - min + 1;
+            int offset = -min;
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                input[i] = new KeyValuePair<int, T>(input[i].Key + offset, input[i].Value);
+            }
+
+
+            min = input.Min(kvp => kvp.Key);
+            max = input.Max(kvp => kvp.Key);
+
+            int exp = 1;
+            while (max / exp > 0)
+            {
+                BasedRadixHelper(input, exp, b);
+                exp *= b;
+            }
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                input[i] = new KeyValuePair<int, T>(input[i].Key - offset, input[i].Value);
+            }
+
+            return input;
+        }
+
+        private static void BasedRadixHelper(List<KeyValuePair<int, T>> input, int exp, int b)
+        {
+            int n = input.Count;
+            List<KeyValuePair<int, T>> output = new List<KeyValuePair<int, T>>(new KeyValuePair<int, T>[n]);
+            int[] count = new int[b]; // b here is radix
+            for (int i = 0; i < n; i++)
+            {
+                count[(input[i].Key / exp) % b]++;
+            }
+            for (int i = 1; i < b; i++)
+            {
+                count[i] += count[i - 1];
+            }
+            for (int i = n - 1; i >= 0; i--) // going backwards
+            {
+                output[count[(input[i].Key / exp) % b] - 1] = input[i];
+                count[(input[i].Key / exp) % b]--;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                input[i] = output[i];
+            }
+        }
+    }
+}
---
+++ RadixSort/RadixSortTest/UnitTest1.cs
@@ -0,0 +1,116 @@
+﻿using RadixSort;
+
+namespace RadixSortTest
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(new int[] { 170, 45, 75, 90, 802, 24, 2, 66 }, new int[] { 2, 24, 45, 66, 75, 90, 170, 802 })]
+        // Negatives test case
+        [InlineData(new int[] { -170, 45, -75, 90, -802, 24, 2, -66 }, new int[] { -802, -170, -75, -66, 2, 24, 45, 90 })]
+        // Duplicates test case
+        [InlineData(new int[] { 170, 45, 75, 90, 802, 24, 2, 66, 45 }, new int[] { 2, 24, 45, 45, 66, 75, 90, 170, 802 })]
+        public void IntRadixSortTest(int[] ints, int[] expected)
+        {
+            List<int> sorted = new List<int>();
+            sorted = TheRadixSort<int>.IntSort(ints.ToList());
+            Assert.Equal(expected, sorted);
+        }
+
+        [Fact]
+        public void RadixSortTest1()
+        {
+            List<KeyValuePair<int, string>> kvps =
+            [
+                new KeyValuePair<int, string>(1, "one"),
+                new KeyValuePair<int, string>(4, "four"),
+                new KeyValuePair<int, string>(1, "uno"),
+                new KeyValuePair<int, string>(2, "two"),
+                new KeyValuePair<int, string>(7, "seven"),
+                new KeyValuePair<int, string>(5, "five"),
+                new KeyValuePair<int, string>(2, "dos")
+            ];
+            List<KeyValuePair<int, string>> expected =
+            [
+                new KeyValuePair<int, string>(1, "one"),
+                new KeyValuePair<int, string>(1, "uno"),
+                new KeyValuePair<int, string>(2, "two"),
+                new KeyValuePair<int, string>(2, "dos"),
+                new KeyValuePair<int, string>(4, "four"),
+                new KeyValuePair<int, string>(5, "five"),
+                new KeyValuePair<int, string>(7, "seven")
+            ];
+            List<KeyValuePair<int, string>> sorted = TheRadixSort<string>.Sort(kvps);
+            Assert.Equal(expected.Count, sorted.Count);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i].Key, sorted[i].Key);
+                Assert.Equal(expected[i].Value, sorted[i].Value);
+            }
+        }
+
+        [Fact]
+        public void RadixSortTest2()
+        {
+            List<KeyValuePair<int, string>> kvps =
+            [
+                new KeyValuePair<int, string>(-1, "n-one"),
+                new KeyValuePair<int, string>(-4, "n-four"),
+                new KeyValuePair<int, string>(-1, "n-uno"),
+                new KeyValuePair<int, string>(-2, "n-two"),
+                new KeyValuePair<int, string>(-7, "n-seven"),
+                new KeyValuePair<int, string>(-5, "n-five"),
+                new KeyValuePair<int, string>(-2, "n-dos")
+            ];
+            List<KeyValuePair<int, string>> expected =
+            [
+                new KeyValuePair<int, string>(-7, "n-seven"),
+                new KeyValuePair<int, string>(-5, "n-five"),
+                new KeyValuePair<int, string>(-4, "n-four"),
+                new KeyValuePair<int, string>(-2, "n-two"),
+                new KeyValuePair<int, string>(-2, "n-dos"),
+                new KeyValuePair<int, string>(-1, "n-one"),
+                new KeyValuePair<int, string>(-1, "n-uno")
+            ];
+            List<KeyValuePair<int, string>> sorted = TheRadixSort<string>.Sort(kvps);
+            Assert.Equal(expected.Count, sorted.Count);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i].Key, sorted[i].Key);
+                Assert.Equal(expected[i].Value, sorted[i].Value);
+            }
+        }
+
+        [Fact]
+        public void RadixSortWithDifferentBaseTest()
+        {
+            List<KeyValuePair<int, string>> kvps =
+            [
+                new KeyValuePair<int, string>(1, "one"),
+                new KeyValuePair<int, string>(4, "four"),
+                new KeyValuePair<int, string>(1, "uno"),
+                new KeyValuePair<int, string>(2, "two"),
+                new KeyValuePair<int, string>(7, "seven"),
+                new KeyValuePair<int, string>(5, "five"),
+                new KeyValuePair<int, string>(2, "dos")
+            ];
+            List<KeyValuePair<int, string>> expected =
+            [
+                new KeyValuePair<int, string>(1, "one"),
+                new KeyValuePair<int, string>(1, "uno"),
+                new KeyValuePair<int, string>(2, "two"),
+                new KeyValuePair<int, string>(2, "dos"),
+                new KeyValuePair<int, string>(4, "four"),
+                new KeyValuePair<int, string>(5, "five"),
+                new KeyValuePair<int, string>(7, "seven")
+            ];
+            List<KeyValuePair<int, string>> sorted = TheRadixSort<string>.VarBaseSort(kvps, 6);
+            Assert.Equal(expected.Count, sorted.Count);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i].Key, sorted[i].Key);
+                Assert.Equal(expected[i].Value, sorted[i].Value);
+            }
+        }
+    }
+}
