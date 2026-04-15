67d06c4661a03a9652efbf8be5b6a8d590877015
Daniel.Sun <danielcysun@gmail.com>
2026-04-14 18:54:57 -0700

    Added and completed BloomFilter

---
+++ BloomFilter/BloomFilter/BloomFilter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloomFilter
+{
+    public class BloomFilter<T>
+    {
+
+        public List<Func<T, int>> hashFunctions;
+
+        public bool[] bitArray;
+
+        public BloomFilter(int cap)
+        {
+            hashFunctions = [HashFuncOne, HashFuncTwo, HashFuncThree];
+            bitArray = new bool[cap];
+        }
+
+
+        public void LoadHashFunc(Func<T, int> hashFunc)
+        {
+            hashFunctions.Add(hashFunc);
+        }
+
+        public void Insert(T item)
+        {
+            foreach (var hashFunc in hashFunctions)
+            {
+                int index = Math.Abs(hashFunc(item) % bitArray.Length);
+                bitArray[index] = true;
+            }
+        }
+
+        public bool ProbablyContains(T item)
+        {
+            foreach (var hashFunc in hashFunctions)
+            {
+                int index = Math.Abs(hashFunc(item) % bitArray.Length);
+                if (!bitArray[index])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private int HashFuncOne(T item)
+        {
+            return item.GetHashCode();
+        }
+
+        private int HashFuncTwo(T item)
+        {
+            string dummyString = "dummystring";
+            return (dummyString, item).GetHashCode();
+        }
+
+        private int HashFuncThree(T item)
+        {
+            int hash = 17;
+            hash *= (HashFuncOne(item), HashFuncTwo(item)).GetHashCode();
+            return hash;
+        }
+    }
+
+}
---
+++ BloomFilter/BloomFilterTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using BloomFilter;
+namespace BloomFilterTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void InsertTest()
+        {
+            BloomFilter<string> bloomFilter = new BloomFilter<string>(1000);
+            bloomFilter.Insert("hello");
+            bloomFilter.Insert("world");
+            Assert.True(bloomFilter.ProbablyContains("hello"));
+            Assert.True(bloomFilter.ProbablyContains("world"));
+            Assert.False(bloomFilter.ProbablyContains("daniel"));
+        }
+
+        [Fact]
+        public void InsertTest2()
+        {
+            BloomFilter<string> bloomFilter = new BloomFilter<string>(1000);
+            bloomFilter.Insert("foo");
+            bloomFilter.Insert("bar");
+            Assert.True(bloomFilter.ProbablyContains("foo"));
+            Assert.True(bloomFilter.ProbablyContains("bar"));
+            Assert.False(bloomFilter.ProbablyContains("baz"));
+        }
+    }
+}
