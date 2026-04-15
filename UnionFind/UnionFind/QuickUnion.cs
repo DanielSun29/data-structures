55f2825dd57271de15011c4266030f3ac4070ee1
Daniel.Sun <danielcysun@gmail.com>
2026-04-14 18:23:26 -0700

    Completed UnionFind

--- UnionFind/UnionFind/QuickUnion.cs
+++ UnionFind/UnionFind/QuickUnion.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace UnionFind
 {
-    internal class QuickUnion<T> : IUnionFind<T>
+    public class QuickUnion<T> : IUnionFind<T>
     {
         Dictionary<T, T> parents; // Maps each element to its parent in the tree
         Dictionary<T, int> items; // Maps each element to its index
@@ -23,17 +23,72 @@ namespace UnionFind
 
         public bool AreConnected(T p, T q)
         {
-            throw new NotImplementedException();
+            if (p == null || q == null) return false;
+
+            int rootP = Find(p);
+            int rootQ = Find(q);
+
+            if (rootP == rootQ)
+            {
+                return true; // They are in the same union
+            }
+            else
+            {
+                return false; // They are in different unions
+            }
         }
 
         public int Find(T p)
         {
-            throw new NotImplementedException();
+            List<T> visited = new List<T>(); // To keep track of visited nodes for path compression
+
+            while (parents[p] != null && !parents[p].Equals(p))
+            {
+                p = parents[p]; // Move up the tree
+            }
+
+            foreach (var node in visited)
+            {
+                parents[node] = p; // Path compression
+            }
+
+            return items[p]; // Return the index of the root element
         }
 
         public bool Union(T p, T q)
         {
-            throw new NotImplementedException();
+            if (p == null || q == null) return false;
+
+            if (AreConnected(p, q))
+            {
+                return false; // They are already in the same union
+            }
+
+            int rootQWeight = 0;
+            int rootPWeight = 0;
+
+            while (parents[q] != null && !parents[q].Equals(q))
+            {
+                rootQWeight++;
+                q = parents[q]; // Move up the tree
+            }
+
+            while (parents[p] != null && !parents[p].Equals(p))
+            {
+                rootPWeight++;
+                p = parents[p]; // Move up the tree
+            }
+
+            if (rootQWeight < rootPWeight)
+            {
+                parents[q] = p; // Attach smaller tree to larger tree
+            }
+            else
+            {
+                parents[p] = q;
+            }
+
+            return true;
         }
     }
 }
---
+++ UnionFind/UnionFindTest/QuickUnionTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using UnionFind;
+
+namespace UnionFindTest
+{
+    public class QuickUnionTest
+    {
+        [Fact]
+        public void AreConnectedTest()
+        {
+            string[] elements = { "A", "B", "C", "D", "E" };
+
+            QuickUnion<string> QuickUnion = new QuickUnion<string>(elements);
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i == j)
+                    {
+                        Assert.True(QuickUnion.AreConnected(elements[i], elements[j]));
+                    }
+                    else
+                    {
+                        Assert.False(QuickUnion.AreConnected(elements[i], elements[j]));
+                    }
+                }
+            }
+
+            QuickUnion.Union("A", "B");
+            QuickUnion.Union("B", "C");
+            QuickUnion.Union("C", "D");
+            QuickUnion.Union("D", "E");
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Assert.True(QuickUnion.AreConnected(elements[i], elements[j]));
+                }
+            }
+        }
+
+        [Fact]
+        public void FindTest()
+        {
+            string[] elements = { "A", "B", "C", "D", "E" };
+            QuickUnion<string> QuickUnion = new QuickUnion<string>(elements);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Assert.Equal(i, QuickUnion.Find(elements[i]));
+            }
+
+            QuickUnion.Union("A", "B");
+            QuickUnion.Union("B", "C");
+            QuickUnion.Union("C", "D");
+            QuickUnion.Union("D", "E");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Assert.Equal(4, QuickUnion.Find(elements[i]));
+            }
+        }
+    }
+}
