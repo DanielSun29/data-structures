8aa7a2c5bc2932fc45a21880d05f458d2a56ff56
Daniel.Sun <danielcysun@gmail.com>
2026-04-14 19:00:52 -0700

    Added LRUCache

---
+++ LRUCache/LRUCache/ICache.cs
@@ -0,0 +1,8 @@
+﻿namespace LRUCache
+{
+    public interface ICache<TKey, TValue>
+    {
+        bool TryGetValue(TKey key, out TValue value);
+        void Put(TKey key, TValue value);
+    }
+}
---
+++ LRUCache/LRUCache/LRUCache.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LRUCache
+{
+    internal class LRUCache<TKey, TValue> : ICache<TKey, TValue>
+    {
+        public LinkedList<TKey> Keys = new LinkedList<TKey>();
+
+        public Dictionary<TKey, TValue> Values = new Dictionary<TKey, TValue>();
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            if (Values.ContainsKey(key))
+            {
+
+            }
+            throw new NotImplementedException();
+        }
+
+        public void Put(TKey key, TValue value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
