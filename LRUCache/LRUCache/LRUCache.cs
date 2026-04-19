55247fd0c91d5c74d2be3b70c2f0821ac8df7c02
DanielSun29 <Daniel.Sun@GMR-118-2-3>
2026-04-19 11:07:15 -0700

    Completed LRUCache

--- LRUCache/LRUCache/LRUCache.cs
+++ LRUCache/LRUCache/LRUCache.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Text;
 
 namespace LRUCache
 {
-    internal class LRUCache<TKey, TValue> : ICache<TKey, TValue>
+    public class LRUCache<TKey, TValue> : ICache<TKey, TValue>
     {
+        public int Capacity;
+
+        private int Count { get { return Values.Count; } }
+
+        public LRUCache(int capacity)
+        {
+            Capacity = capacity;
+        }
+
+        public LRUCache()
+        {
+            Capacity = 100;
+        }
+
         public LinkedList<TKey> Keys = new LinkedList<TKey>();
 
         public Dictionary<TKey, TValue> Values = new Dictionary<TKey, TValue>();
@@ -14,14 +29,35 @@ namespace LRUCache
         {
             if (Values.ContainsKey(key))
             {
-
+                Keys.Remove(key);
+                Keys.AddFirst(key);
+                value = Values[key];
+                return true;
             }
-            throw new NotImplementedException();
+            value = default;
+            return false;
         }
 
         public void Put(TKey key, TValue value)
         {
-            throw new NotImplementedException();
+            if (Count >= Capacity)
+            {
+                var lastKey = Keys.Last.Value;
+                Keys.RemoveLast();
+                Values.Remove(lastKey);
+            }
+
+            if (!Values.ContainsKey(key))
+            {
+                Values.Add(key, value);
+                Keys.AddFirst(key);
+            }
+            else
+            {
+                Keys.Remove(key);
+                Keys.AddFirst(key);
+                Values[key] = value;
+            }
         }
     }
 }
---
+++ LRUCache/LRUCacheTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using LRUCache;
+
+namespace LRUCacheTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void LRUCacheTest1()
+        {
+            LRUCache<int, int> cache = new LRUCache<int, int>(10);
+
+            for (int i = 0; i < 10; i++)
+            {
+                cache.Put(i, i);
+                Assert.True(cache.TryGetValue(i, out int value1));
+                Assert.Equal(i, value1);
+            }
+
+            cache.Put(10, 10);
+            Assert.False(cache.TryGetValue(0, out int value2));// 0 should be evicted
+
+            Assert.False(cache.TryGetValue(11, out int value3)); // 11 doesn't exist
+        }
+    }
+}
