9cb90a902f1713bbbd9cc7ac7ecd980097764dd6
DanielSun29 <Daniel.Sun@GMR-118-2-3>
2026-05-12 18:59:57 -0700

    Added Red Black Tree

---
+++ RedBlackTree/RedBlackTree/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedBlackTree
+{
+    public class Node<T> where T : IComparable<T>
+    {
+        public bool IsRed;
+
+        public T Value;
+
+        public Node<T> Left;
+        public Node<T> Right;
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public Node(T value, bool isRed)
+        {
+            Value = value;
+            IsRed = isRed;
+        }
+    }
+}
---
+++ RedBlackTree/RedBlackTree/RBTree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedBlackTree
+{
+    public class RBTree<T> where T : IComparable<T>
+    {
+        public Node<T> Root;
+
+        public Node<T> LeftRotate(Node<T> node)
+        {
+            var newRoot = node.Right;
+            node.Right = newRoot.Left;
+            newRoot.Left = node;
+            node.IsRed = true;
+            newRoot.IsRed = false;
+            return newRoot;
+        }
+
+        public Node<T> RightRotate(Node<T> node)
+        {
+            var newRoot = node.Left;
+            node.Left = newRoot.Right;
+            newRoot.Right = node;
+            node.IsRed = true;
+            newRoot.IsRed = false;
+            return newRoot;
+        }
+
+        public void FlipColor(Node<T> node)
+        {
+            node.IsRed = !node.IsRed;
+            if (node.Left != null)
+                node.Left.IsRed = !node.Left.IsRed;
+            if (node.Right != null)
+                node.Right.IsRed = !node.Right.IsRed;
+        }
+
+        public bool isRed(Node<T> node)
+        {
+            return node != null && node.IsRed;
+        }
+    }
+}
