d3ed292248714b2cca0c46a5dcfb90ff91baff02
DanielSun29 <danielcysun@gmail.com>
2026-05-17 17:28:26 -0700

    Added Sorted Set(The last one!)

---
+++ SortedSet/SortedSet/SortedSet.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortedSet
+{
+    public class SortedSet<T> : ISortedSet<T>
+    {
+        public IComparer<T> Comparer => throw new NotImplementedException();
+
+        public int Count => throw new NotImplementedException();
+
+        public bool Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<T> items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Ceiling(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Floor(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISortedSet<T> Intersection(ISortedSet<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Max()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Min()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISortedSet<T> Union(ISortedSet<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
