f760ed43428acdb868bcdaaf4e5fba5bda536f11
DanielSun29 <danielcysun@gmail.com>
2026-05-17 17:19:57 -0700

    Completed Red Black Tree

--- RedBlackTree/RedBlackTree/RBTree.cs
+++ RedBlackTree/RedBlackTree/RBTree.cs
@@ -8,7 +8,183 @@ namespace RedBlackTree
     {
         public Node<T> Root;
 
-        public Node<T> LeftRotate(Node<T> node)
+
+        public RBTree()
+        {
+            Root = null;
+        }
+
+        public Node<T> Search(T value)
+        {
+            Node<T> current = Root;
+            while (current != null)
+            {
+                if (value.CompareTo(current.Value) < 0)
+                {
+                    current = current.Left;
+                }
+                else if (value.CompareTo(current.Value) > 0)
+                {
+                    current = current.Right;
+                }
+                else if (value.CompareTo(current.Value) == 0)
+                {
+                    return current;
+                }
+            }
+            return null;
+        }
+
+        public bool Contains(T value)
+        {
+            return Search(value) != null;
+        }
+
+        public void Insert(T value)
+        {
+            Root = Insert(Root, value);
+            Root.IsRed = false;
+        }
+
+        private Node<T> Insert(Node<T> node, T value)
+        {
+            if (node == null)
+            {
+                return new Node<T>(value) { IsRed = true };
+            }
+
+            if (isRed(node.Left) && isRed(node.Right))
+            {
+                FlipColor(node);
+            }
+
+            if (value.CompareTo(node.Value) < 0)
+            {
+                node.Left = Insert(node.Left, value);
+            }
+            else if (value.CompareTo(node.Value) > 0)
+            {
+                node.Right = Insert(node.Right, value);
+            }
+
+            node = FixUp(node);
+
+            return node;
+        }
+
+        public void Remove(T value)
+        {
+            if (Root == null) return;
+            Root = Delete(Root, value);
+            if (Root != null)
+            {
+                Root.IsRed = false;
+            }
+        }
+
+        private Node<T> Delete(Node<T> node, T value)
+        {
+            if (value.CompareTo(node.Value) < 0)
+            {
+                if (node.Left != null)
+                {
+                    if (!isRed(node.Left) && !isRed(node.Left.Left))
+                    {
+                        node = MoveRedLeft(node);
+                    }
+                    node.Left = Delete(node.Left, value);
+                }
+            }
+            else
+            {
+                if (isRed(node.Left))
+                {
+                    node = RightRotate(node);
+                }
+                if (value.CompareTo(node.Value) == 0 && node.Right == null)
+                {
+                    return null;
+                }
+                if (node.Right != null)
+                {
+                    if (!isRed(node.Right) && !isRed(node.Right.Left))
+                    {
+                        node = MoveRedRight(node);
+                    }
+
+                    if (value.CompareTo(node.Value) == 0)
+                    {
+                        DeleteNode(node);
+                    }
+                    else
+                    {
+                        node.Right = Delete(node.Right, value);
+                    }
+                }
+            }
+
+
+            node = FixUp(node);
+            return node;
+        }
+
+        private void DeleteNode(Node<T> node)
+        {
+            if (node.Right == null)
+            {
+                return;
+            }
+
+            Node<T> min = node.Right;
+            while (min.Left != null)
+            {
+                min = min.Left;
+            }
+
+            node.Value = min.Value;
+            node.Right = Delete(node.Right, min.Value);
+        }
+
+        private Node<T> FixUp(Node<T> node)
+        {
+            if (isRed(node.Right) && !isRed(node.Left))
+            {
+                node = LeftRotate(node);
+            }
+            if (isRed(node.Left) && isRed(node.Left.Left))
+            {
+                node = RightRotate(node);
+            }
+            return node;
+        }
+
+        private Node<T> MoveRedRight(Node<T> node)
+        {
+            FlipColor(node);
+            if (node.Left is not null)
+            {
+                if (isRed(node.Left.Left))
+                {
+                    node = RightRotate(node);
+                    FlipColor(node);
+                }
+            }
+            return node;
+        }
+
+        private Node<T> MoveRedLeft(Node<T> node)
+        {
+            FlipColor(node);
+            if (isRed(node.Right.Left))
+            {
+                node.Right = RightRotate(node.Right);
+                node = LeftRotate(node);
+                FlipColor(node);
+            }
+            return node;
+        }
+
+        private Node<T> LeftRotate(Node<T> node)
         {
             var newRoot = node.Right;
             node.Right = newRoot.Left;
@@ -18,7 +194,7 @@ namespace RedBlackTree
             return newRoot;
         }
 
-        public Node<T> RightRotate(Node<T> node)
+        private Node<T> RightRotate(Node<T> node)
         {
             var newRoot = node.Left;
             node.Left = newRoot.Right;
@@ -28,7 +204,7 @@ namespace RedBlackTree
             return newRoot;
         }
 
-        public void FlipColor(Node<T> node)
+        private void FlipColor(Node<T> node)
         {
             node.IsRed = !node.IsRed;
             if (node.Left != null)
---
+++ RedBlackTree/RedBlackTreeTest/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using RedBlackTree;
+
+namespace RedBlackTreeTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void SimpleInsertTest()
+        {
+            RBTree<int> tree = new RBTree<int>();
+            tree.Insert(1);
+            tree.Insert(2);
+
+            Assert.True(tree.Contains(1));
+            Assert.True(tree.Contains(2));
+        }
+
+        [Fact]
+        public void RandomInsertTest()
+        {
+            RBTree<int> tree = new RBTree<int>();
+            List<int> values = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                int value = Random.Shared.Next(1, 100);
+                values.Add(value);
+                tree.Insert(value);
+            }
+            foreach (int value in values)
+            {
+                Assert.True(tree.Contains(value));
+            }
+        }
+
+        [Fact]
+        public void SimpleRemoveTest()
+        {
+            RBTree<int> tree = new RBTree<int>();
+            tree.Insert(1);
+            tree.Insert(2);
+            tree.Insert(3);
+
+            tree.Remove(2);
+            Assert.False(tree.Contains(2));
+            Assert.True(tree.Contains(1));
+            Assert.True(tree.Contains(3));
+        }
+
+        [Fact]
+        public void RandomRemoveTest()
+        {
+            RBTree<int> tree = new RBTree<int>();
+            List<int> values = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                int value = Random.Shared.Next(1, 100);
+                values.Add(value);
+                tree.Insert(value);
+            }
+            foreach (int value in values)
+            {
+                tree.Remove(value);
+                Assert.False(tree.Contains(value));
+            }
+        }
+    }
+}
